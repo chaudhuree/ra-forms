1e161d219ea936a0a2f5011ea97fc8b20bb42e88
chaudhuree <chaudhuree00@gmail.com>
2022-11-04 01:06:42 +0600

    completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,166 +1,129 @@
-import React, { useState } from 'react'
-import './Form.css'
+import React, { useState } from "react";
+import "./Form.css";
 function Form() {
   const [data, setData] = useState({
-    billFirstName: "",
-    billLastName: "",
-    billAddress1: "",
-    billAddress2: "",
-    billCity: "",
-    billState: "",
-    billZipCode: ""
-  })
-  const handleSubmit = e => {
-    e.preventDefault()
+    FirstName: "",
+    LastName: "",
+    Address1: "",
+    Address2: "",
+    City: "",
+    State: "",
+    ZipCode: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(JSON.stringify(data));
-  }
-  const handleChange = e => {
-    const type=e.target.type;
-    const name=e.target.name;
-    const value=e.target.type==='checkbox'?e.target.checked:e.target.value;
-  }
+  };
+  const handleChange = (e) => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const { Address2, ...otherProps } = data;
+  const allFilled = [...Object.values(otherProps)].every(Boolean);
   return (
     <div>
-      <from className='form flex-col'
-        onSubmit={handleSubmit}
-      >
+      <form className="form flex-col" onSubmit={handleSubmit}>
         <div className="flex-col">
           <div className="split-container">
             <div className="flex-col">
-              <label htmlFor="billFirstName">First Name</label>
+              <label htmlFor="FirstName">First Name</label>
               <input
                 type="text"
-                id="billFirstName"
-                name="billFirstName"
+                id="FirstName"
+                name="FirstName"
                 placeholder="shahriar"
                 pattern="([A-Z])[\w+.]{1,}"
-                value={data.billFirstName}
+                value={data.FirstName}
                 onChange={handleChange}
               />
             </div>
             <div className="flex-col">
-              <label htmlFor="billLastName">Last Name</label>
+              <label htmlFor="LastName">Last Name</label>
               <input
                 type="text"
-                id="billLastName"
-                name="billLastName"
+                id="LastName"
+                name="LastName"
                 placeholder="kabir"
                 pattern="([A-Z])[\w+.]{1,}"
-                value={data.billLastName}
+                value={data.LastName}
                 onChange={handleChange}
               />
             </div>
           </div>
 
-          <label htmlFor="billAddress1">Address</label>
+          <label htmlFor="Address1">Address</label>
           <input
             type="text"
-            id="billAddress1"
-            name="billAddress1"
+            id="Address1"
+            name="Address1"
             placeholder="dakkhinkhan dhaka"
             pattern="[\w\d\s.#]{2,}"
-            value={data.billAddress1}
+            value={data.Address1}
             onChange={handleChange}
           />
 
-          <label htmlFor="billAddress2" className="offscreen">2nd Address line</label>
+          <label htmlFor="Address2" className="offscreen">
+            2nd Address line
+          </label>
           <input
             type="text"
-            id="billAddress2"
-            name="billAddress2"
+            id="Address2"
+            name="Address2"
             placeholder="Apt. 2"
             pattern="[\w\d\s.#]{2,}"
-            value={data.billAddress2}
+            value={data.Address2}
             onChange={handleChange}
           />
 
-          <label htmlFor="billCity">City</label>
+          <label htmlFor="City">City</label>
           <input
             type="text"
-            id="billCity"
-            name="billCity"
+            id="City"
+            name="City"
             placeholder="Dhaka"
             pattern="([A-Z])[\w\s.]{1,}"
-            value={data.billCity}
+            value={data.City}
             onChange={handleChange}
           />
 
-          <label htmlFor="billState">State</label>
+          <label htmlFor="State">State</label>
           <select
-            id="billState"
-            name="billState"
-            value={data.billState}
+            id="State"
+            name="State"
+            value={data.State}
             onChange={handleChange}
           >
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District Of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
+            <option value="NONE">Select State</option>
+            <option value="UT">Uttara</option>
+            <option value="MP">Mirpur</option>
+            <option value="AZ">Azompur</option>
+            <option value="DM">Dhanmondi</option>
+            <option value="NM">Newmarket</option>
+            <option value="KP">Komlapur</option>
+            <option value="JP">Joydebpur</option>
           </select>
 
-          <label htmlFor="billZipCode">Zip Code</label>
+          <label htmlFor="ZipCode">Zip Code</label>
           <input
             type="text"
-            id="billZipCode"
-            name="billZipCode"
+            id="ZipCode"
+            name="ZipCode"
             placeholder="12345"
             pattern="[0-9]{5}"
             maxLength="5"
-            value={data.billZipCode}
+            value={data.ZipCode}
             onChange={handleChange}
           />
         </div>
-        <button type='submit' className="button" >Submit</button>
-      </from>
+        <button type='submit' className="button" disabled={!allFilled}>
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
-// disabled={!canSave}
-export default Form
+
+export default Form;
